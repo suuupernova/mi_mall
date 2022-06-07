b72e3850dad9eccc046e260ef21af659be80613a
suuupernova <39875475+suuupernova@users.noreply.github.com>
2022-06-07 23:30:09 +0800

    路由封装

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import Vue from 'vue'
+// import router from './router'
+// import App from './App.vue'
+// Vue.config.productionTip=false
+// new Vue({
+//     router,
+//     render:h=>h(App),
+
+// }).$mount('#app')
+
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './router'
+createApp(App).use(router).mount('#app')
