7aa2f2aa2fc93f3b1d9155141e8a6a99cb11cef5
suuupernova <39875475+suuupernova@users.noreply.github.com>
2022-06-10 00:52:05 +0800

    接口环境设置

--- src/main.js
+++ src/main.js
@@ -15,12 +15,15 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import env from './env'
 
 createApp(App).use(router).use(VueAxios,axios).mount('#app')
 
 //根据前端的跨域方式做调整
-axios.defaults.baseURL='/api';
+axios.defaults.baseURL='http://test-www.imooc.com/api';
 axios.defaults.timeout=8000;
+//根据环境变量获取不同的请求地址
+axios.defaults.baseURL=env.baseURL;
 //接口错误拦截
 axios.interceptors.response.use(function(response){
     let res = response.data;
