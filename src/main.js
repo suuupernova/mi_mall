c2e8141247890bb51c95b21aac4666eec44ec54e
suuupernova <39875475+suuupernova@users.noreply.github.com>
2022-06-10 00:06:37 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -13,4 +13,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-createApp(App).use(router).mount('#app')
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+createApp(App).use(router).use(VueAxios,axios).mount('#app')
+
+//根据前端的跨域方式做调整
+axios.defaults.baseURL='/api';
+axios.defaults.timeout=8000;
+//接口错误拦截
+axios.interceptors.response.use(function(response){
+    let res = response.data;
+    if(res.status==0){
+        return res.data;
+    }
+    else if(res.status==10){
+        window.location.href='/#/login';
+    }else{
+        alert(res.msg);
+    }
+});
