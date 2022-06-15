4c428dd104abde4fdfff04bc319aab94e410a66e
suuupernova <39875475+suuupernova@users.noreply.github.com>
2022-06-16 00:51:22 +0800

    nav-header

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -9,9 +9,10 @@
                     <a href="javascript:;">协议规则</a>
                 </div>
                 <div class="topbar-user">
-                    <a href="javascript:;">登录</a>
-                    <a href="javascript:;">注册</a>
-                    <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+                    <a href="javascript:;" v-if="username">{{username}}</a>
+                    <a href="javascript:;" v-if="!username" @click="login()">登录</a>
+                    <a href="javascript:;">我的订单</a>
+                    <a href="javascript:;" class="my-cart" @click="goToCart()"><span class="icon-cart"></span>购物车</a>
                 </div>
             </div>            
         </div>
@@ -25,21 +26,84 @@
                     <div class="item-menu">
                         <span>小米手机</span>
                         <div class="children">
-
+                            <ul>
+                                <li class="product" v-for="(item,index) in phoneList" :key="index">
+                                    <a v-bind:href="'/#/product/'+item.id" target="_blank">
+                                        <div class="pro-img">
+                                            <img :src="item.mainImage" :alt="item.subtitle">
+                                        </div>
+                                        <div class="pro-name">{{item.name}}</div>
+                                        <div class="pro-price">{{item.price | currency}}</div>
+                                    </a>
+                                </li>
+                                
+                            </ul>
                   
                         </div>
                     </div>
                     <div class="item-menu">
                         <span>Redmi红米</span>
-                        <div class="children">
-
-                  
-                        </div>
+                        
                     </div>
                     <div class="item-menu">
                         <span>电视</span>
                         <div class="children">
-
+                            <ul>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="../../public/imgs/nav-img/nav-3-1.jpg" alt="">
+                                        </div>
+                                        <div class="pro-name">小米全面屏电视E55A</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="../../public/imgs/nav-img/nav-3-2.jpg" alt="">
+                                        </div>
+                                        <div class="pro-name">小米全面屏电视E55A</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="../../public/imgs/nav-img/nav-3-3.png" alt="">
+                                        </div>
+                                        <div class="pro-name">小米全面屏电视E55A</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="../../public/imgs/nav-img/nav-3-4.jpg" alt="">
+                                        </div>
+                                        <div class="pro-name">小米全面屏电视E55A</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="../../public/imgs/nav-img/nav-3-5.jpg" alt="">
+                                        </div>
+                                        <div class="pro-name">小米全面屏电视E55A</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="../../public/imgs/nav-img/nav-3-6.png" alt="">
+                                        </div>
+                                        <div class="pro-name">小米全面屏电视E55A</div>
+                                        <div class="pro-price">1999元</div>
+                                    </a>
+                                </li>
+                            </ul>
                   
                         </div>
                     </div>
@@ -56,12 +120,47 @@
 </template>
 <script>
     export default{
-        name:'nav-header'
+        name:'nav-header',
+        data(){
+            return{
+                username:'jack',
+                phoneList:[]
+            }
+        },
+        filters:{
+            currency(val){
+                if(!val)return '0.00';
+                return '¥'+val.toFixed(2)+'元'
+            }
+        },
+        mounted(){
+            this.getProductionList();
+        },
+        methods:{
+            login(){
+                this.$router.push('/login');
+            },
+            getProductionList(){
+                this.axios.get('/products',{
+                    params:{
+                        categoryId:'100012',
+                    }
+                }).then((res)=>{
+                    if(res.list.length>6){
+                        this.phoneList=res.list.slice(0,6);
+                    }
+                });
+            },
+            goToCart(){
+                this.$router.push('/cart');
+            }
+        }
     }
 </script>
 <style lang="scss">
 @import '../assets/scss/base.scss';
 @import '../assets/scss/mixin.scss';
+@import '../assets/scss/config.scss';
     .header{
         .nav-topbar{
             height: 39px;
@@ -94,6 +193,7 @@
         }
         .nav-header{
             .container{
+                position: relative;
                 height: 112px;
                 @include flex();
                 .header-logo{
@@ -107,22 +207,22 @@
                         height: 55px;
                         &:before{
                             content: '';
-                            //@include bgImg(55px,55px,'../../public/imgs/mi-logo.png',55px);
-                            display: inline-block;
-                            width: 55px;
-                            height: 55px;
-                            background: url('../../public/imgs/mi-logo.png') no-repeat center;
-                            background-size: 55px;
+                            @include bgImg(55px,55px,'../../public/imgs/mi-logo.png',55px);
+                            // display: inline-block;
+                            // width: 55px;
+                            // height: 55px;
+                            // background: url('../../public/imgs/mi-logo.png') no-repeat center;
+                            // background-size: 55px;
                              transition: margin .2s;
                         }
                         &:after{
                             content: '';
-                            //@include bgImg(55px,55px,'../../public/imgs/mi-home.png',55px);
-                            display: inline-block;
-                            width: 55px;
-                            height: 55px;
-                            background: url('../../public/imgs/mi-home.png') no-repeat center;
-                            background-size: 55px;
+                            @include bgImg(55px,55px,'../../public/imgs/mi-home.png',55px);
+                            // display: inline-block;
+                            // width: 55px;
+                            // height: 55px;
+                            // background: url('../../public/imgs/mi-home.png') no-repeat center;
+                            // background-size: 55px;
                         }
                         &:hover::before{
                             margin-left: -55px;
@@ -145,7 +245,68 @@
                             cursor: pointer;
                         }
                         &:hover{
+                            color: $colorA;
+                            .children{
+                                height: 220px;
+                                opacity: 1;
+                            }
+                        }
+                        .children{
+                            position: absolute;
+                            top: 112px;
+                            left: 0px;
+                            width: 1226px;
+                            height: 0;
+                            opacity: 0;
+                            overflow: hidden;
+                            border-top: 1px solid #E5E5E5;
+                            box-shadow: 0px 7px 6px 0px rgba(0,0,0,0.11);
+                            z-index: 10;
+                            transition: all .5s;
+                            //background-color: #ffffff;
                             
+                            .product{
+                                position: relative;
+                                float: left;
+                                width: 16.6%;
+                                height: 220px;
+                                font-size: 12px;
+                                line-height: 12px;
+                                text-align: center;
+                                a{
+                                    display: inline-block;
+                                }
+                                img{
+                                    width: auto;
+                                    height: 111px;
+                                    margin-top: 26px;
+                                }
+                                .pro-img{
+                                    height: 137px;
+                                }
+                                .pro-name{
+                                    font-weight: bold;
+                                    margin-top: 19px;
+                                    margin-bottom: 8px;
+                                    color: $colorB;
+                                   
+                                }
+                                .pro-price{
+                                    color: $colorA;
+                                }
+                                &:before{
+                                content: "";
+                                position: absolute;
+                                top: 28px;
+                                right: 0;
+                                border-left: 1px solid $colorF;
+                                height: 100px;
+                                width: 1px;
+                                }
+                                &:last-child:before{
+                                    display: none;
+                                }
+                            }
                         }
                     }
                 }
