13a46438b684f91af8e4830b4c676f209f1631e9
suuupernova <39875475+suuupernova@users.noreply.github.com>
2022-06-17 00:09:53 +0800

    swiper

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-        
+    <div class="index">
+        <div class="container">
+            <div class="swiper">
+                <swiper v-bind:options="swiperOption">
+                    <swiper-slide v-for="(item,index) in slideList" v-bind:key="index">
+                        <a v-bind:href="'/#/product/'+item.id"><img v-bind:src="item.img" alt=""></a>
+                    </swiper-slide>
+                    <!-- <div class="swiper-pagination" slot="pagination"></div>
+                    <div class="swiper-button-prev" slot="button-prev"></div>
+                    <div class="swiper-button-next" slot="button-next"></div>
+                     -->
+                     <!-- <div class="swiper-pagination"></div> -->
+                </swiper>
+            </div>
+            <div class="ads"></div>
+            <div class="banner"></div>
+            
+            <div class="product-box"></div>
+        </div>
 
         <service-bar></service-bar>
 
@@ -8,14 +25,95 @@
 
 </template>
 <script>
-
+    import 'swiper/css';
+    //import { Pagination } from 'swiper';
+    import { Swiper, SwiperSlide } from 'swiper/vue';
     import ServiceBar from './../components/ServiceBar'
     export default{
         name:'index',
         components:{
 
             ServiceBar,
+            Swiper,
+            SwiperSlide,
+            
+        },
+        setup(){
+            const onSwiper=(swiper)=>{
+                console.log(swiper);
+            };
+            const onSlideChange = ()=>{
+                console.log('slide change');
+            };
+            return {
+                onSwiper,
+                onSlideChange,
+            };
+        },
+        data(){
+            return{
+                swiperOption:{
+                    autoplay:true,
+                    loop:true,
+                    effect:'cube',
+                    cubeEffect:{
+                        slideShadows:true,
+                        shadow:true,
+                        shadowOffsets:100,
+                        shadowScale:0.6
+                    },
+                },
+                slideList:[
+                    {
+                        id:'42',
+                        img:'/imgs/slider/slide-1.jpg'
+                    },
+                    {
+                        id:'45',
+                        img:'/imgs/slider/slide-2.jpg'
+                    },
+                    {
+                        id:'46',
+                        img:'/imgs/slider/slide-3.jpg'
+                    },
+                    {
+                        id:'',
+                        img:'/imgs/slider/slide-4.jpg'
+                    },
+                    {
+                        id:'',
+                        img:'/imgs/slider/slide-5.jpg'
+                    },
+                ]
+            }
+        }
+    }
+    /* eslint-disable */
+    // var mySwiper = new Swiper('.swiper',{
+    //     effect : 'cube',
+    //     cubeEffect: {
+    //     slideShadows: true,
+    //     shadow: true,
+    //     shadowOffset: 100,
+    //     shadowScale: 0.6
+    //     },
+        // })
+  
+</script>
+<style lang="scss">
+    .index{
+        .swiper{
+            
+                
+                height: 451px;
+                img{
+                    width: 100%;
+                }
+           
+            
+        
+            
             
         }
     }
-</script>
+</style>
