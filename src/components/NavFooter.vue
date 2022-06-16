42f88648111a06358465bc03f6c598a9f2b182ca
suuupernova <39875475+suuupernova@users.noreply.github.com>
2022-06-16 17:38:26 +0800

    nav-footer

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="../../public/imgs/logo-footer.png" alt="">
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="">选购指南</a><span>|</span>
+            <a href="">服务中心</a><span>|</span>
+            <a href="">线下门店</a><span>|</span>
+            <a href="">关于小米</a><span>|</span>
+            <a href="">关注我们</a>
+        </div>
+        <div class="copyright">Copytight ©2022 <span>mi.future.com</span> All Rights Reserved</div>
     </div>
 </template>
 <script>
@@ -8,3 +19,44 @@
         name:'nav-footer'
     }
 </script>
+<style lang="scss" scoped>
+    @import '../assets/scss/base.scss';
+    @import '../assets/scss/mixin.scss';
+    @import '../assets/scss/config.scss';
+    .footer{
+        height: 234px;
+        border-top: 4px solid $colorA;
+        background-color: $colorB;
+        color: $colorD;
+        font-size: $fontI;
+        text-align: center;
+        .footer-logo{
+            margin-top: 46px;
+            margin-bottom: 31px;
+            img{
+                width: 53px;
+                height: 36px;
+                margin-bottom: 13px;
+            }
+        }
+        .footer-link{
+            a{
+                color: $colorD;
+                display: inline-block;
+            }
+            span{
+                margin: 0 10px;
+
+            }
+
+        }
+        .copyright{
+            margin-top: 13px;
+            span{
+                color: $colorA;
+            }
+        }
+    }
+    
+
+</style>
--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -263,7 +263,7 @@
                             box-shadow: 0px 7px 6px 0px rgba(0,0,0,0.11);
                             z-index: 10;
                             transition: all .5s;
-                            //background-color: #ffffff;
+                            background-color: #ffffff;
                             
                             .product{
                                 position: relative;
