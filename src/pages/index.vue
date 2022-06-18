5705ab48791b6fd0d09caea2e0f4c0b8347ee164
suuupernova <39875475+suuupernova@users.noreply.github.com>
2022-06-18 23:52:00 +0800

    首页广告位

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -78,8 +78,16 @@
                 </swiper>
                 
             </div>
-            <div class="ads"></div>
-            <div class="banner"></div>
+            <div class="ads-box">
+                <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" v-bind:key="index">
+                    <img v-bind:src="item.img" alt="">
+                </a>
+            </div>
+            <div class="banner">
+                <a href="/#/product/30">
+                    <img src="/imgs/banner-1.png" alt="">
+                </a>
+            </div>
             
             <div class="product-box"></div>
         </div>
@@ -167,6 +175,24 @@
                         }
                     ],
                     [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+                ],
+                adsList:[
+                    {
+                        id:33,
+                        img:'/imgs/ads/ads-1.png'
+                    },
+                    {
+                        id:48,
+                        img:'/imgs/ads/ads-2.jpg'
+                    },
+                    {
+                        id:45,
+                        img:'/imgs/ads/ads-3.png'
+                    },
+                    {
+                        id:47,
+                        img:'/imgs/ads/ads-4.jpg'
+                    }
                 ]
             }
         }
@@ -260,6 +286,18 @@
                     
                 } 
             }  
+            .ads-box{
+                @include flex();
+                margin-top: 14px;
+                margin-bottom: 31px;
+                a{
+                    width: 296px;
+                    height: 167px;
+                }
+            }
+            .banner{
+                margin-bottom: 50px;
+            }
                     
             
         }
